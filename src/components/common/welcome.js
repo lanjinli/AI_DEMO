5723f11d4e28910e26320b15cbc3c1a0ebc2abc5
lanjinli <li.jin@qq.com>
2018-12-14 17:22:42 +0800

    新增欢迎页

--- src/components/common/welcome.js
+++ src/components/common/welcome.js
@@ -1,48 +1,128 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    StatusBar,
+    Image,
+    TouchableOpacity,
+    NativeModules
+} from 'react-native';
+import {
+    Demensions,
+    ScreenWidth,
+    ScreenHeight,
+    STATUS_BAR_HEIGHT,
+    NAVBSR_HEIGHT
+} from '../../constants/util';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class Welcome extends Component {
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+    //跳转首页
+    getHomePage() {
+        this.props.navigation.replace('HomeDrawerNav');
+    }
+    static navigationOptions = {
+        // 隐藏头部
+        header: () => null
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            countdown: 3
+        }
+    }
+
+    componentDidMount() {
+        // 3秒后跳转到Home
+        this.timer = setInterval(() => {
+            if (this.state.countdown <= 0) {
+                this.getHomePage();
+            } else {
+                this.setState({
+                    countdown: this.state.countdown - 1
+                })
+            }
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        this.timer && clearInterval(this.timer);
+    }
+    
+    render() {
+        return (
+            <View style={[styles.flex]}>
+                <StatusBar
+                    animated={true}//变化是否动画过渡
+                    hidden={true}//是否显示
+                    backgroundColor={'#000'}//背景颜色
+                    translucent={true}//状态栏是否透明
+                    networkActivityIndicatorVisible={false}//ios 状态加载中
+                    showHideTransition={'fade'}
+                    barStyle={'dark-content'} // 文本颜色
+                />
+                <Image source={require("../../assets/image/welcome.jpg")} style={styles.adsImg}/>
+                <View style={styles.slogan}>
+                    <Text style={styles.slogan_text}>腾讯AI黑科技 一键可达</Text>
+                </View>
+                <TouchableOpacity style={[styles.skip]} onPress={this.getHomePage.bind(this)}>
+                    <Text style={styles.skip_text}>{this.state.countdown}</Text>
+                    <Text style={styles.skip_text}> | </Text>
+                    <Text style={styles.skip_text}>跳过</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flex: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+        backgroundColor: '#fff'
+    },
+    adsImg: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        width: ScreenWidth,
+        height: null,
+        resizeMode: 'cover'
+    },
+    slogan: {
+        width: ScreenWidth,
+        height: ScreenHeight/5,
+        backgroundColor: '#FFF',
+        position: 'absolute',
+        bottom: 0,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    slogan_text: {
+        fontSize: 16,
+        color: '#666',
+    },
+    skip: {
+        position: 'absolute',
+        top: STATUS_BAR_HEIGHT,
+        right: 10,
+        width: 80,
+        height: 28,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0,0,0,0.3)',
+        borderRadius: 14,
+    },
+    skip_text: {
+        color: '#FFF',
+        fontSize: 14,
+    }
 });
---
+++ src/constants/util.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+    Platform,
+    PixelRatio,
+    NativeModules
+} from 'react-native';
+// import Toast from 'react-native-root-toast';
+
+export const Demensions = require('Dimensions');
+export const ScreenWidth = Demensions.get('screen').width;
+export const ScreenHeight = Demensions.get('screen').height;
+export const Scale = Demensions.get('screen').scale;
+
+const X_WIDTH = 375;
+const X_HEIGHT = 812;
+const { StatusBarManager } = NativeModules;
+
+export function isIphoneX() {
+    return (
+        Platform.OS === 'ios' && ((height === X_HEIGHT && width === X_WIDTH) || (height === X_WIDTH && width === X_HEIGHT))
+    )
+}
+
+export function ifIphoneX (iphoneXStyle, regularStyle) {
+    if (isIphoneX()) {
+        return iphoneXStyle;
+    } else {  
+        return regularStyle;
+    }
+}
+
+export const STATUS_BAR_HEIGHT = Platform.select({
+    ios: ifIphoneX(44,20),
+    android: StatusBarManager.HEIGHT
+});
+
+export const NAVBSR_HEIGHT = Platform.select({
+    ios: 44,
+    android: 50,
+});
+
+// let toast;
+// export const toastUtil = (content, time = 'SHORT') => {
+//     if (toast !== undefined) {
+//       Toast.hide(toast);
+//     }
+//     toast = Toast.show(content.toString(), {
+//         duration: Toast.durations[time],
+//         position: Toast.positions.BOTTOM,
+//         animation: true,
+//         shadow: false,
+//         backgroundColor: 'rgba(0,0,0,.8)',
+//         hideOnPress: true,
+//         delay: 0,
+//         opacity: 1,
+//         containerStyle: {
+//             paddingHorizontal: 10,
+//             paddingVertical: 6,
+//             borderRadius: 3,
+//         },
+//         textStyle: {
+//             fontSize: 14,
+//             lineHeight: 18,
+//         },
+//     });
+// };
